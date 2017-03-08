92dba74cfd6506764a0150a7d8e4f8e2813ee79f
John Bencic <sales@networkbytes.com.au>
2017-03-08 22:22:55 +1100

    added comments in xmlparsing

--- xmlparsing.rb
+++ xmlparsing.rb
@@ -3,7 +3,11 @@ require 'nokogiri'
 xml_file = File.read("#{File.dirname(__FILE__)}/sample_files/sample.xml")
 
 doc = Nokogiri::XML.parse(xml_file)
-puts doc
+#puts doc
+
+# xpath can be //* for all elements /sitcom or /drama
+# Note: anything begining with // will pull the whole tree and not just the value
+
 doc.xpath('//*').each do
 |sitcom_element|
   puts "\nName : "+sitcom_element.xpath('name').text
